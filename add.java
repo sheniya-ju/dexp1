8a385d24c1373fdfe47f73c972c29e88788dfa8e
Sheniya <145789837+sheniya-ju@users.noreply.github.com>
2025-05-09 19:35:33 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sheniya
+ */
+public class add {
+    
+    public static void main(String[] args) {
+        int num1 = 10; // first number
+        int num2 = 20; // second number
+        int sum = num1 + num2; // addition
+
+        System.out.println("The sum is: " + sum);
+    }
+
+   
+}
