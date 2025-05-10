802a0c2f3455f856815093f96fc81d801364a55e
Sheniya <145789837+sheniya-ju@users.noreply.github.com>
2025-05-10 13:32:44 +0530

    Update add.java

--- add.java
+++ add.java
@@ -9,13 +9,19 @@
  */
 public class add {
     
-    public static void main(String[] args) {
-        int num1 = 10; // first number
-        int num2 = 20; // second number
-        int sum = num1 + num2; // addition
-
-        System.out.println("The sum is: " + sum);
+    static int factorial(int n)
+    {
+        int res = 1, i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
     }
 
-   
+    
+    public static void main(String[] args)
+    {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is "
+                           + factorial(5));
+    }
 }
