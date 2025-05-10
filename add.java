4b335eb8d635f9293e8a37c0e4e153ddf0c744d7
Sheniya <145789837+sheniya-ju@users.noreply.github.com>
2025-05-10 13:34:34 +0530

    fact.java

--- add.java
+++ add.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Sheniya
- */
 public class add {
     
     static int factorial(int n)
